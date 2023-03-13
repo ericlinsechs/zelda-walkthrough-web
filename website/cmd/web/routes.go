9219d541604a9613d6a29cf6b9af02ed0ed97ae6
Eric <lin.ericcy@inventec.com>
2023-03-13 16:28:48 +0800

    Put image in view page.

--- website/cmd/web/routes.go
+++ website/cmd/web/routes.go
@@ -12,8 +12,6 @@ func (app *application) routes(r *gin.Engine) *gin.Engine {
 	r.GET("/", app.home)
 	r.GET("/armor/list", app.armorList)
 	r.GET("/armor/view/:id", app.armorView)
-	// r.GET("/users/list", app.usersList)
-	// r.GET("/users/view/:id", app.usersView)
 
 	// static path
 	r.Static("/static/", "../../ui/static")
